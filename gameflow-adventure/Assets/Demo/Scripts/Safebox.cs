19498fa7b0fe08950b91093c413c1e4758feb86b
blueloveTH <blueloveTH@qq.com>
2020-05-16 17:37:41 +0800

    up

--- gameflow-adventure/Assets/Demo/Scripts/Safebox.cs
+++ gameflow-adventure/Assets/Demo/Scripts/Safebox.cs
@@ -3,26 +3,24 @@ using UnityEngine;
 
 public class Safebox : MonoBehaviour
 {
+    public enum Flag
+    {
+        Stage1, Stage2, Stage3, Stage4, Success, Failure
+    }
+
     public FlowDiagram fd;
 
     void Start()
     {
-        fd = new FlowDiagram();
+        fd = FlowDiagram.CreateByEnum<Flag>();
 
-        FlowNode stage1 = fd.CreateNode("stage1");
-        FlowNode stage2 = fd.CreateNode("stage2");
-        FlowNode stage3 = fd.CreateNode("stage3");
-        FlowNode stage4 = fd.CreateNode("stage4");
-        FlowNode success = fd.CreateNode("success");
-        FlowNode failure = fd.CreateNode("failure");
+        fd[Flag.Success].onEnter += (x) => fd.isLocked = true;
 
-        success.onEnter += (x) => fd.isLocked = true;
-
-        var t12 = stage1.AddArc(stage2).AddData("code", "1L");
-        var t23 = stage2.AddArc(stage3).AddData("code", "3R");
-        var t35 = stage3.AddArc(success).AddData("code", "2L");
-        var t24 = stage2.AddArc(stage4).AddData("code", "2R");
-        var t45 = stage4.AddArc(success).AddData("code", "3L");
+        var t12 = fd[Flag.Stage1].AddArc(fd[Flag.Stage2]).AddData("code", "1L");
+        var t23 = fd[Flag.Stage2].AddArc(fd[Flag.Stage3]).AddData("code", "3R");
+        var t35 = fd[Flag.Stage3].AddArc(fd[Flag.Success]).AddData("code", "2L");
+        var t24 = fd[Flag.Stage2].AddArc(fd[Flag.Stage4]).AddData("code", "2R");
+        var t45 = fd[Flag.Stage4].AddArc(fd[Flag.Success]).AddData("code", "3L");
 
         fd.onCurrentNodeChange += (x) => print(x.name + " (OnNodeChange)");
 
@@ -36,13 +34,13 @@ public class Safebox : MonoBehaviour
             item.exitTaskCreator +=
                 () => Task.Delay(2f).OnComplete(() => print("exit task end."));
         }
-        fd.Enter("stage1");
+        fd.Enter(Flag.Stage1);
     }
 
     public void Input(string str)
     {
         var t = fd.currentNode?.FindArc((arc) => arc.GetData<string>("code") == str);
         if (t != null) fd.Enter(t.target);
-        else fd.Enter("failure");
+        else fd.Enter(Flag.Failure);
     }
 }
