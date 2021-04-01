212a0e95ef81e3e170991e394a66926d4e6b4ca7
blueloveTH <blueloveTH@qq.com>
2021-04-01 23:51:58 +0800

    up

--- gameflow-adventure/Assets/com.bl.gameflow/Runtime/Task/Ex/WaitEventTask.cs
+++ gameflow-adventure/Assets/com.bl.gameflow/Runtime/Task/Ex/WaitEventTask.cs
@@ -5,17 +5,35 @@ namespace GameFlow
     public sealed class WaitEventTask : Task
     {
         UnityEvent e;
+        bool tag = false;
 
         internal WaitEventTask(UnityEvent e)
         {
             this.e = e;
+        }
+
+        protected override void OnPlay()
+        {
+            base.OnPlay();
             e.AddListener(Complete);
+            tag = true;
         }
 
         protected override void OnComplete()
         {
             e.RemoveListener(Complete);
+            tag = false;
             base.OnComplete();
+        }
+
+        protected override void OnKill()
+        {
+            if (tag)
+            {
+                e.RemoveListener(Complete);
+                tag = false;
+            }
+            base.OnKill();
         }
 
     }
